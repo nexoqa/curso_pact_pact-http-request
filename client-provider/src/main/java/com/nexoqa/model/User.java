120f4df3a08a727f9b42ef0f9b63fa345686fa6a
Jose Ignacio Ojeda <jojedava@gmail.com>
2022-06-21 11:22:45 +0200

    exercise 21062022

--- client-provider/src/main/java/com/nexoqa/model/User.java
+++ client-provider/src/main/java/com/nexoqa/model/User.java
@@ -12,6 +12,7 @@ public class User {
     private String name;
     private String lastName;
     private String address;
+    private String email;
     private Integer age;
     private Integer phoneNumber;
 
